2f92b3377ff49af3d536356d78d2735397663e87
Asif Jalil <62372647+asif-jalil@users.noreply.github.com>
2021-06-07 02:16:22 +0600

    fixing bug

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -3,22 +3,23 @@ import { Card, Container } from 'react-bootstrap';
 import { Link, useHistory } from 'react-router-dom';
 
 const Profile = () => {
-   const [userProfile, setUserProfile] = useState({})
+   const history = useHistory();
+   const [userProfile, setUserProfile] = useState({});
    const currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
-   const history = useHistory()
-   
+
    useEffect(() => {
       fetch("https://parseapi.back4app.com/users/me", {
          method: "GET",
          headers: {
-            "X-Parse-Application-Id": "1aKNPKlZZkL3gKe1avjN4bJzq1pkgAw7vj2aLRO3",
+            "X-Parse-Application-Id":
+               "1aKNPKlZZkL3gKe1avjN4bJzq1pkgAw7vj2aLRO3",
             "X-Parse-REST-API-Key": "ibV2rrC1p90TfLxwUumNkKAzI7Rgk49bCj4ODnLV",
-            "X-Parse-session-token": currentUser
-         }
+            "X-Parse-session-token": currentUser,
+         },
       })
-         .then(res => res.json())
-         .then(data=> setUserProfile(data))
-   }, [currentUser])
+         .then((res) => res.json())
+         .then((data) => setUserProfile(data));
+   }, [currentUser]);
 
    const handleLogout = () => {
       sessionStorage.setItem("currentUser", '');
